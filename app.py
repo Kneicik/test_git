f50591b5c23c6112ea57f3371adf20399c029f29
Filip <knajcikpl@gmail.com>
2025-10-06 17:37:08 +0200

    Add function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-liczba1 = int(input("poodaj liczbe1: "))
-liczba2 = int(input("poodaj liczbe2: "))
-
-print("suma: ", liczba1 + liczba2)
+def dodawanie():
+    liczba1 = int(input("poodaj liczbe1: "))
+    liczba2 = int(input("poodaj liczbe2: "))
+    
+    print("suma: ", liczba1 + liczba2)
